b072ef41be780f6b5d62ed78800a193c94f5c8e1
dag217 <dag217@lehigh.edu>
2014-09-02 01:23:45 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+//David Galaydick
+//9/1/2014
+//CSE002
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args){
+        
+        //print a statement in a box
+        System.out.println("-----------");
+        System.out.println("| Welcome |");
+        System.out.println("-----------");
+        
+        //print Lehigh ID in some fancy spiky box thing
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-d--a--g--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        
+        //print out a Tweet-length autobiographical statement
+        System.out.println("Once upon a last year, I had a beard. Then I made the unfortunate decision of getting a job that involved physical labor over the summer, so I had to shave it off to keep myself cool. Good times.");
+      
+    }
+}
