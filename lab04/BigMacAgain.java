0275d90d775a694bd17602b6ba2f3ace1e407e82
dag217 <dag217@lehigh.edu>
2014-09-19 13:51:47 +0000

    I fixed it

--- lab04/BigMacAgain.java
+++ lab04/BigMacAgain.java
@@ -21,12 +21,19 @@ public class BigMacAgain{
         double cost_bigmac = 2.22;
         double cost_fries = 2.15;
         
-        int num_bigmacs; //initializing the big mac variable for future use
+        //initializing various variables for future use
+        int num_bigmacs; 
+        int pretotal_dollars;
+        int pretotal_dimes;
+        int pretotal_pennies;
+        double total;
+        
         
         //ask user to input number of big macs
         System.out.print("Insert number of Big Macs to be purchased: ");
         if (scanner.hasNextInt()){ //checking if the user is about to enter an integer
             num_bigmacs = scanner.nextInt(); //a logical inputs an integer
+            total = num_bigmacs*cost_bigmac; // calculating the total
         }
         
         //making sure user put in an integer
@@ -34,14 +41,6 @@ public class BigMacAgain{
             System.out.println("Banana is not an amount of delicious Big Macs you can purchase."); //error message
             return; //terminate program
         }
-        
-        double total = num_bigmacs*cost_bigmac; // calculating the total
-        
-        int pretotal_dollars = (int)(total); //pretotal dollar value
-        int pretotal_dimes = (int)(total*10)%10; //pretotal dime value
-        int pretotal_pennies = (int)(total*100)%10; //pretotal penny value
-        
-        System.out.println("In case you forgot, you ordered " +num_bigmacs+ " for $" +pretotal_dollars+ "." +pretotal_dimes+pretotal_pennies+ "."); //tells the user how much the big macs they be buying and how much it is so far.
         
             
         if (num_bigmacs < 0){
@@ -52,28 +51,34 @@ public class BigMacAgain{
         //checking if the user entered a positive number
         if (num_bigmacs >= 0){ //the number has to be greater than or equal to zero
         
-        //asks user if they want fries
-        System.out.print("Would you like fries with that? (Y/y/N/n): ");
-        String response = scanner.next(); //user inputs one of the recommended replies, hopefully
+
+            pretotal_dollars = (int)(total); //pretotal dollar value
+            pretotal_dimes = (int)(total*10)%10; //pretotal dime value
+            pretotal_pennies = (int)(total*100)%10; //pretotal penny value
+            System.out.println("In case you forgot, you ordered " +num_bigmacs+ " for $" +pretotal_dollars+ "." +pretotal_dimes+pretotal_pennies+ ".");
         
-        //checking if the user wanted fries
-        if (response.equals("Y") || response.equals("y")){ //checking for positive replies
-            System.out.println("You ordered fries for an addition $2.15.");
-            total += cost_fries; //adding fries to their total
+            //asks user if they want fries
+            System.out.print("Would you like fries with that? (Y/y/N/n): ");
+            String response = scanner.next(); //user inputs one of the recommended replies, hopefully
+        
+            //checking if the user wanted fries
+            if (response.equals("Y") || response.equals("y")){ //checking for positive replies
+                System.out.println("You ordered fries for an addition $2.15.");
+                total += cost_fries; //adding fries to their total
+                
+            }
             
-        }
-        
-        //checking if user didn't want fries
-        else if (response.equals("N") || response.equals("n")){ //checking for negative replies
-            System.out.println("Fine. More fries for us!"); //snarking
+            //checking if user didn't want fries
+            else if (response.equals("N") || response.equals("n")){ //checking for negative replies
+                System.out.println("Fine. More fries for us!"); //snarking
+                
+            }
             
-        }
-        
-        //checking if user can follow directions
-        else {
-            System.out.println("#fail, You had to put Y, y, N, or n!"); //error message
-            return; //program terminates
-        }
+            //checking if user can follow directions
+            else {
+                System.out.println("#fail, You had to put Y, y, N, or n!"); //error message
+                return; //program terminates
+            }
         
         } //end of the if statement that checked for positive number
             
